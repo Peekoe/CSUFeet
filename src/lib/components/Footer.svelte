fc9d3183ecc5a84f43bfd95b056fc8cc73fdd9b7
Charlie Taylor <peekoecode@gmail.com>
2022-09-20 00:26:41 -0700

    Start with landing

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer class="footer">
+  <div class="content">
+    <span>
+      <img src="src/feet.svg" alt="feet logo" height="24" width="24" />
+      <strong style="margin-left:3px;">CSUFeet 2022</strong> 
+    </span>
+  </div>
+</footer>
+
+<style>
+  .footer {
+    padding: 2rem 1.5rem;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
+  import Footer from "$lib/components/Footer.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
 </script>
 
-<a href="/home">Home</a>
-<a href="/upload">Upload</a>
+<body>
+  <Navbar />
 
-<h1>Welcome to CSUFeet!</h1>
+  <section class="hero is-large is-info" id="hero">
+    <div class="hero-body">
+      <p class="title">
+        CSUFeet
+      </p>
+      <p class="subtitle">
+        The best place to post feet online
+      </p>
+    </div>
+  </section>
+
+  <Footer />  
+</body>
+
+
+<style>
+   body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+  }
+
+  #hero {
+    background-color: darkred;
+  }
+
+  .title {
+    font-size: 2.5rem;
+  }
+</style>
