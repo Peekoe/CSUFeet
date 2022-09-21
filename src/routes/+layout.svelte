0ea8ce80b52e0f17f11517c2bd852d2dea95818c
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-21 09:20:25 -0800

    Let them construct a +layout.svelte for Me, that I may dwell among them (Exodus 25:8)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Navbar from '$lib/components/Navbar.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<body>
+  <header>
+    <Navbar />
+  </header>
+
+  <slot />
+
+  <footer>
+    <Footer />
+  </footer>
+</body>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+  }
+
+  header,
+  footer {
+    flex: none;
+  }
+</style>
