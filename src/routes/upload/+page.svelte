47f1605a1db0d92b8ec3384c2c36be514c2c0226
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-22 17:37:40 -0800

    Let them construct a +layout.svelte for Me, that I may dwell among them (Exodus 25:8) (#14)

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-  import Footer from '$lib/components/Footer.svelte';
-  import Navbar from '$lib/components/Navbar.svelte';
   import UploadForm from '$lib/components/UploadForm.svelte';
 </script>
 
-<Navbar />
-
 <UploadForm />
-
-<Footer />
